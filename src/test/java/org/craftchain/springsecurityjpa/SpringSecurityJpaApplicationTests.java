59d04f6af35aca4a858357709e8c58513cde23ad
Anicet Togue <atogue@gmail.com>
2021-01-08 17:42:05 +0100

    disable default test until running a mysql db instance.

--- src/test/java/org/craftchain/springsecurityjpa/SpringSecurityJpaApplicationTests.java
+++ src/test/java/org/craftchain/springsecurityjpa/SpringSecurityJpaApplicationTests.java
@@ -1,8 +1,10 @@
 package org.craftchain.springsecurityjpa;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("disable until running a mysql db instance!")
 @SpringBootTest
 class SpringSecurityJpaApplicationTests {
 
