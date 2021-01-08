0840074896173ef58d7e98f091365a3f6feec302
Anicet Togue <atogue@gmail.com>
2021-01-08 17:34:11 +0100

    wiring user repository and throwing exception on load user by username

--- src/main/java/org/craftchain/springsecurityjpa/service/MyUserDetailsService.java
+++ src/main/java/org/craftchain/springsecurityjpa/service/MyUserDetailsService.java
@@ -1,14 +1,26 @@
 package org.craftchain.springsecurityjpa.service;
 
 import org.craftchain.springsecurityjpa.model.MyUserDetails;
+import org.craftchain.springsecurityjpa.model.User;
+import org.craftchain.springsecurityjpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Optional;
+
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new MyUserDetails(s);
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+        return user.map(MyUserDetails::new).get();
     }
 }
